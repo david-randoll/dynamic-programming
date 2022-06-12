ad1984d4b322dd4b673278651b91762caa8e549d
d-randoll <51640895+d-randoll@users.noreply.github.com>
2022-06-12 18:44:58 -0400

    fib implementation and time and space analysis

---
+++ fib/fib.js
@@ -0,0 +1,16 @@
+const fib = (n) => {
+    if (n <= 2) return 1;
+    return fib(n - 1) + fib(n - 2);
+};
+
+console.log("fib(1): ", fib(1)); //1
+console.log("fib(2): ", fib(2)); //1
+console.log("fib(3): ", fib(3)); //2
+console.log("fib(4): ", fib(4)); //3
+console.log("fib(50): ", fib(50)); //12586269025
+
+/*
+    Time and space complexity:
+
+    See fib.drawio
+*/
